1d455d634a28c62923b340c84023631097121057
Dedeepyaijjapureddi <dedeepyaijjapureddi@gmail.com>
2025-12-05 11:56:38 +0530

    new changes

---
+++ express.js
@@ -0,0 +1,11 @@
+import express from'express';
+const app=express();
+app.get('/users',(req,res) =>{
+    res.send("hello this is dedeepya");
+})
+app.get('/get-users', (req,res) => {
+    res.send("hello");
+})
+app.listen(7001,()=>{
+    console.log("server running at port 7001")
+});
